751b373af8eed56f2a9f59f257c46f5b4b80f078
ANUBHAV MISHRA <133557776+anubhav-0004@users.noreply.github.com>
2024-03-01 01:21:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        dblue : "#02042A",
+        darkblue : "#181c2e",
+        lightblue : "#2377c9",
+        lightblue1 : "#0746a9"
+      },
+    },
+  },
+  plugins: [],
+}
+
